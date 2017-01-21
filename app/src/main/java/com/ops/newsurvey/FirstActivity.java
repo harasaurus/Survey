a361e7acaff196e1c579d3e7f4420614b892fa7a
harasaurus <harshit@fusionstrings.com>
2017-01-21 21:00:57 +0530

    Update 1.2: Added login and signup activity.

--- app/src/main/java/com/ops/newsurvey/FirstActivity.java
+++ app/src/main/java/com/ops/newsurvey/FirstActivity.java
@@ -1,7 +1,11 @@
 package com.ops.newsurvey;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        Button log=(Button) findViewById(R.id.login);
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(FirstActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button sup=(Button) findViewById(R.id.signup);
+        sup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(FirstActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/ops/newsurvey/MainActivity.java
+++ app/src/main/java/com/ops/newsurvey/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ops.newsurvey;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nextstep();
+    }
+
+    public void nextstep(){
+        //// TODO:Check if user have opened app for first time if yes 'rookie'=true else false
+    boolean rookie=true;
+        if(rookie){
+            Intent ontent=new Intent(this,FirstActivity.class);
+            startActivity(ontent);
+        }
+        else{
+            Intent notent=new Intent(this,HomeActivity.class);
+            startActivity(notent);
+        }
     }
 }
